810449edca14cdf547aa8e94c68fd8af118329ef
plok74122 <plok74122@hotmail.com>
2016-03-25 15:00:22 +0800

    完成張貼資訊與資訊類別的model建立 並完成關聯設定

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325063016) do
+ActiveRecord::Schema.define(version: 20160325065608) do
+
+  create_table "article_types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "article_type_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
