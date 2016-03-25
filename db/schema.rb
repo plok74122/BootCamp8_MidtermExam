519a133aa0ed1089634e4709dfe94ed3cf076ebe
plok74122 <plok74122@hotmail.com>
2016-03-25 16:02:06 +0800

    完成留言的model 與 controller 並完成個資料庫的關聯

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325065608) do
+ActiveRecord::Schema.define(version: 20160325075628) do
 
   create_table "article_types", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20160325065608) do
     t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "article_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
