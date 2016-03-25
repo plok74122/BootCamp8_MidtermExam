238f19ce5b21d5f5d17347a47afdc5e6a3ee24b7
plok74122 <plok74122@hotmail.com>
2016-03-25 15:54:21 +0800

    可以在首頁瀏覽並分頁所有訊息，顯示訊息的標題和張貼時間

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!
   before_action :article_params , :only =>[:create , :update]
+  before_action :article_id_params , :only =>[:edit ,:update ,:show]
+  before_action :set_article, :only => [:show, :edit, :update]
 
   def index
     @articles =Article.all
@@ -10,11 +12,16 @@ class ArticlesController < ApplicationController
     @articles = Article.new
     @all_article_type_collection = ArticleType.all
   end
+
   def create
     new_article = Article.new(article_params)
     new_article.user_id = current_user.id
     flash[:notice] = new_article.save
     redirect_to articles_path
+  end
+
+  def show
+
   end
 
   private
@@ -27,4 +34,7 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :content, :article_type_id )
   end
 
+  def article_id_params
+    @article_id = params.permit(:id)
+  end
 end
--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,5 +1,5 @@
 class InformationController < ApplicationController
   def index
-
+    @articles =Article.page(params[:page]).per(5)
   end
 end
